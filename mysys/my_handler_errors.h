12a210d1a192b91741b23c24c8d12b03e93e3491
Rich Prohaska <prohaska@tokutek.com>
2013-03-27 15:01:13 +0000

    refs #6089 add disk full error code
    git-svn-id: file:///svn/mysql.com/mysql-5.5.30@54777 c7de825b-a66e-492c-adef-691d508d4ae1

--- mysys/my_handler_errors.h
+++ mysys/my_handler_errors.h
@@ -84,7 +84,8 @@ static const char *handler_error_messages[]=
   "Index column length exceeds limit",
   "Index corrupted",
   "Undo record too big",
-  "Table is being used in foreign key check"
+  "Table is being used in foreign key check",
+  "Disk full",
 };
 
 extern void my_handler_error_register(void);
