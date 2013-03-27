64ddd0b9485cde89031999c9a66974340570b505
Rich Prohaska <prohaska@tokutek.com>
2013-03-27 15:21:26 +0000

    refs #6089 clustering key grammar
    git-svn-id: file:///svn/mysql.com/mysql-5.5.30@54781 c7de825b-a66e-492c-adef-691d508d4ae1

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -249,7 +249,7 @@ public:
 
 class Key :public Sql_alloc {
 public:
-  enum Keytype { PRIMARY, UNIQUE, MULTIPLE, FULLTEXT, SPATIAL, FOREIGN_KEY};
+  enum Keytype { PRIMARY, UNIQUE, MULTIPLE, FULLTEXT, SPATIAL, FOREIGN_KEY, CLUSTERING};
   enum Keytype type;
   KEY_CREATE_INFO key_create_info;
   List<Key_part_spec> columns;
