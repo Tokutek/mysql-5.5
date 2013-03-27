03db4e4e28dfd6f8d0d125342edaa3a9a1f16fe1
Rich Prohaska <prohaska@tokutek.com>
2013-03-27 15:41:16 +0000

    refs #6089 add tokudb compression options to row format
    git-svn-id: file:///svn/mysql.com/mysql-5.5.30@54790 c7de825b-a66e-492c-adef-691d508d4ae1

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -6844,6 +6844,8 @@ get_row_format_name(
 	case ROW_TYPE_PAGE:
 	case ROW_TYPE_NOT_USED:
 		break;
+    default:
+        break;
 	}
 	return("NOT USED");
 }
@@ -6969,6 +6971,7 @@ create_options_are_valid(
 	case ROW_TYPE_FIXED:
 	case ROW_TYPE_PAGE:
 	case ROW_TYPE_NOT_USED:
+    default:
 		push_warning(
 			thd, MYSQL_ERROR::WARN_LEVEL_WARN,
 			ER_ILLEGAL_HA_CREATE_OPTION,		\
@@ -7193,6 +7196,7 @@ ha_innobase::create(
 	case ROW_TYPE_NOT_USED:
 	case ROW_TYPE_FIXED:
 	case ROW_TYPE_PAGE:
+    default:
 		push_warning(
 			thd, MYSQL_ERROR::WARN_LEVEL_WARN,
 			ER_ILLEGAL_HA_CREATE_OPTION,
