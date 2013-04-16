b38e740dd06654bdc4d63ab0f61dca53b7c1fd23
Bradley C. Kuszmaul <bradley@tokutek.com>
2013-04-16 22:04:42 +0000

    #6598.  Fix the tokubackup_version variable.
    git-svn-id: file:///svn/mysql.com/mysql-5.5.30@55588 c7de825b-a66e-492c-adef-691d508d4ae1

--- sql/sql_backup.h
+++ sql/sql_backup.h
@@ -1,7 +1,6 @@
 #include <mysqld_error.h>
-#include "backup.h"
 
 extern int sql_backups(const char *source_dir, const char *dest_dir, THD *thd);
 extern void sql_backup_throttle(unsigned long rate);
 
-const extern char tokubackup_version[];
+const extern char *tokubackup_version;
