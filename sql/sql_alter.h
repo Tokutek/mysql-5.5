815f044bd709c3944614d0d3f0562715e6250e60
Rich Prohaska <prohaska@tokutek.com>
2013-10-27 05:45:39 -0400

    refs #6089 5.6 inplace alter table

--- sql/sql_alter.h
+++ sql/sql_alter.h
@@ -63,4 +63,86 @@ public:
   bool execute(THD *thd);
 };
 
+/** Runtime context for ALTER TABLE. */
+class Alter_table_ctx
+{
+public:
+  Alter_table_ctx();
+
+  Alter_table_ctx(THD *thd, TABLE_LIST *table_list, uint tables_opened_arg,
+                  char *new_db_arg, char *new_name_arg);
+
+  /**
+     @return true if the table is moved to another database, false otherwise.
+  */
+  bool is_database_changed() const
+  { return (new_db != db); };
+
+  /**
+     @return true if the table is renamed, false otherwise.
+  */
+  bool is_table_renamed() const
+  { return (is_database_changed() || new_name != table_name); };
+
+  /**
+     @return filename (including .frm) for the new table.
+  */
+  const char *get_new_filename() const
+  {
+    DBUG_ASSERT(!tmp_table);
+    return new_filename;
+  }
+
+  /**
+     @return path to the original table.
+  */
+  const char *get_path() const
+  {
+    DBUG_ASSERT(!tmp_table);
+    return path;
+  }
+
+  /**
+     @return path to the new table.
+  */
+  const char *get_new_path() const
+  {
+    DBUG_ASSERT(!tmp_table);
+    return new_path;
+  }
+
+  /**
+     @return path to the temporary table created during ALTER TABLE.
+  */
+  const char *get_tmp_path() const
+  { return tmp_path; }
+
+public:
+  Create_field *datetime_field;
+  bool         error_if_not_empty;
+  uint         tables_opened;
+  char         *db;
+  char         *table_name;
+  char         *alias;
+  char         *new_db;
+  char         *new_name;
+  char         *new_alias;
+  char         tmp_name[80];
+
+private:
+  char new_filename[FN_REFLEN + 1];
+  char new_alias_buff[FN_REFLEN + 1];
+  char path[FN_REFLEN + 1];
+  char new_path[FN_REFLEN + 1];
+  char tmp_path[FN_REFLEN + 1];
+
+#ifndef DBUG_OFF
+  /** Indicates that we are altering temporary table. Used only in asserts. */
+  bool tmp_table;
+#endif
+
+  Alter_table_ctx &operator=(const Alter_table_ctx &rhs); // not implemented
+  Alter_table_ctx(const Alter_table_ctx &rhs);            // not implemented
+};
+
 #endif
