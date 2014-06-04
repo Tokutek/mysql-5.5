ea0217461bce70e2cf8b3b1465a47005c6012af3
Rich Prohaska <prohaska@tokutek.com>
2014-06-04 13:43:49 -0400

    #37 define DB_TYPE_TOKUDB

--- sql/handler.h
+++ sql/handler.h
@@ -326,7 +326,8 @@ enum legacy_db_type
   DB_TYPE_MARIA,
   /** Performance schema engine. */
   DB_TYPE_PERFORMANCE_SCHEMA,
-  DB_TYPE_FIRST_DYNAMIC=42,
+  DB_TYPE_TOKUDB=42,
+  DB_TYPE_FIRST_DYNAMIC=43,
   DB_TYPE_DEFAULT=127 // Must be last
 };
 
@@ -713,6 +714,7 @@ struct handlerton
     This is going away and new engines will just use "name" for this.
   */
   enum legacy_db_type db_type;
+
   /*
     each storage engine has it's own memory area (actually a pointer)
     in the thd, for storing per-connection information.
